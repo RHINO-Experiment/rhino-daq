ce4977df1b4d95055e634ec8edf6656070244b45
jmnorris98 <133128882+jmnorris98@users.noreply.github.com>
2025-03-04 20:37:04 +0000

    Updated vna to be about 500 kHz

--- rhino_daq_rtlsdr.py
+++ rhino_daq_rtlsdr.py
@@ -30,8 +30,8 @@ class VNA:
         scan_s11_mean, scan_s11_std = np.mean(scan_s11, axis=0), np.std(scan_s11, axis=0)
         time.sleep(0.1)
 
-        vna.set_sweep(start=50e6, stop=52e6)
-        vna.set_frequencies(start=50e6, stop=52e6)
+        vna.set_sweep(start=500e3, stop=600e3)
+        vna.set_frequencies(start=500e3, stop=600e3)
         vna.pause()
         
     
@@ -362,23 +362,133 @@ class MultiFrequencyObserver:
         save_file.close()
         pass
 
+
+    def begin_observations_no_vna(self, obs_title=None):
+        date_string = datetime.now().strftime("%Y_%m_%d")
+        time_string = datetime.now().strftime("%H%M%S")
+        save_file = h5py.File(self.save_folder+date_string+"no_vna.hdf5" ,'a')
+        int_time_per_freq = timedelta(seconds=self.integration_time_per_frequency)
+        epoch = datetime.now()
+
+        if obs_title is None:
+            obs_group = save_file.create_group(time_string)
+        else:
+            obs_group = save_file.create_group(obs_title)
+            # add in option to check if group exists and add_1 to the name
+
+        for cf in self.centre_frequencies:
+            freq_string = 'f'+str(cf / 10**6)
+            freq_group = obs_group.create_group(freq_string)
+            sdr = RtlSdrLogger(self.averaging_time_per_sdr_sample, self.sample_rate, cf,
+                               fft_length=self.fft_length, sdr_gain_set = self.sdr_gain,
+                               window_function=self.spectrum_window, averaging=True)
+            
+            #solf_array = self.get_sol_measurements(start_f=cf-self.sample_rate/2, end_f=cf+self.sample_rate/2, nint=10)
+            #freq_group.create_dataset('sol_f_arr', data=solf_array, dtype=solf_array.dtype)
+
+            self.switches.set_switch_state('vna_antenna')
+            #vna = VNA()
+            #antenna_s11, _, vna_freqs = vna.get_s11_measurements(min_freq=cf-self.sample_rate/2, max_freq=cf+self.sample_rate/2,
+            #                                                     n_integrations=10)
+            #antenna_s11 = np.array(antenna_s11)
+            #freq_group.create_dataset('s11_ant',data=antenna_s11, dtype=antenna_s11.dtype)
+
+            self.switches.set_switch_state('vna_load_term')
+            #load_s11,_,_ = vna.get_s11_measurements(min_freq=cf-self.sample_rate/2, max_freq=cf+self.sample_rate/2,
+            #                                        n_integrations=10)
+            #load_s11 = np.array(load_s11)
+            #freq_group.create_dataset('s11_load',data=load_s11, dtype=load_s11.dtype)
+
+            self.switches.set_switch_state('vna_noise_diode')
+            #noise_diode_s11,_,_ = vna.get_s11_measurements(min_freq=cf-self.sample_rate/2, max_freq=cf+self.sample_rate/2,
+            #                                                n_integrations=10)
+            #noise_diode_s11 = np.array(noise_diode_s11)
+
+            #rec_s11,_,_ = vna.get_s11_measurements(min_freq=cf-self.sample_rate/2, max_freq=cf+self.sample_rate/2,
+            #                                                n_integrations=10)
+            #rec_s11 = np.array(rec_s11)
+            #freq_group.create_dataset('s11_rec', data=rec_s11, dtype=rec_s11.dtype)
+            #freq_group.create_dataset('s11_noise_diode', data=noise_diode_s11, dtype=noise_diode_s11.dtype)
+            #vna_freqs = np.array(vna_freqs)
+            #freq_group.create_dataset('s11_vna_freqs', data=vna_freqs, dtype=vna_freqs.dtype)
+
+            t_0 = datetime.now()
+            obs_switch_states = ["rec_antenna", "rec_load_term", "rec_noise_diode"]
+
+            sdr.init_sdr()
+
+            if self.obs_time_split is None:
+                switch_time_det = [t_0 + int_time_per_freq/3, t_0 + 2*int_time_per_freq/3, t_0 + int_time_per_freq]
+            else:
+                switch_time_det = [t_0 + self.obs_time_split[0]*int_time_per_freq, 
+                               t_0 + (self.obs_time_split[0]+self.obs_time_split[1])*int_time_per_freq,
+                               t_0 + int_time_per_freq]
+            
+
+            switch_state = []
+            switch_times_real = []
+            temperatures = []
+            times = []
+            spectra = []
+            for i in np.arange(len(obs_switch_states)):
+                data_log = True
+                self.switches.set_switch_state(obs_switch_states[i])
+                switch_state.append(obs_switch_states[i])
+                switch_times_real.append(datetime.now())
+
+                while data_log:
+                    t = datetime.now()
+                    temp = self.thermometry.read_temp()
+                    s = sdr.get_avg_spectra()
+                    spectra.append(s)
+                    temperatures.append(temp)
+                    times.append(t)
+                    
+                    if datetime.now() >= switch_time_det[i]:
+                        data_log=False
+            
+            spectra = np.array(spectra)
+            frequencies = np.linspace(cf-self.sample_rate/2, cf+self.sample_rate/2, num=self.fft_length)
+            frequencies = np.array(frequencies)
+            temperatures = np.array(temperatures)
+            times = [(t - epoch).total_seconds() for t in times]
+            times = np.array(times)
+            switch_times_real = [(t - epoch).total_seconds() for t in switch_times_real]
+            switch_times_real = np.array(switch_times_real)
+            epcoh_string = str(epoch.strftime('%Y_%m_%d_%H%M%S'))
+
+            freq_group.create_dataset('spectra', data=spectra, dtype=spectra.dtype)
+            freq_group.create_dataset('spectra_frequencies', data=frequencies, dtype=frequencies.dtype)
+            t_ds = freq_group.create_dataset('times', data=times, dtype=times.dtype)
+            st_ds = freq_group.create_dataset('switch_times', data=switch_times_real, dtype=switch_times_real.dtype)
+            freq_group.create_dataset('temperatures', data=temperatures, dtype=temperatures.dtype)
+
+            t_ds.attrs['epoch'] = epcoh_string
+            st_ds.attrs['epoch'] = epcoh_string
+            for switch_state in np.arange(len(obs_switch_states)):
+                st_ds.attrs['switch_state_'+str(int(switch_state))] = obs_switch_states[switch_state]
+            print(str(cf)+' done...')
+
+        save_file.close()
+        pass
+
 if __name__ == '__main__':
     switch_dictionary = {"vna_short":"1_1_7",
                         "vna_load":"1_1_6",
                         "vna_open":"1_1_3",
                         "vna_antenna":"1_1_1",
                         "vna_load_term":"1_1_2",
-                        "vna_noise_diode":"1_1_5",
+                        "vna_noise_diode":"1_1_4",
                         "rec_antenna":"1_2_1",
                         "rec_load_term":"1_2_2",
-                        "rec_noise_diode":"1_2_5",
+                        "rec_noise_diode":"1_2_4",
                         "vna_rec":"2_2_8"
                         }
     switch_thermo = Switch_Thermometry(com_port='COM3', baud_rate=115200, switch_dictionary=switch_dictionary)
 
     observer = MultiFrequencyObserver(thermometry=switch_thermo, switches=switch_thermo, freq_range=(60e6, 80e6), 
                                       sample_rate=2.0e6, averaging_time_per_sdr_sample=0.1, fft_length=2048, sdr_gain=0.0,
-                                      spectrum_window='Blackman', integration_time_per_frequency=30, obs_time_split=[1/3,1/3,1/3],
+                                      spectrum_window='Blackman', integration_time_per_frequency=300, obs_time_split=[1/3,1/3,1/3],
                                       save_folder='')
    
-    observer.begin_observations()
+    observer.begin_observations_no_vna()
